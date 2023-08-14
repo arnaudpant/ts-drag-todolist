856b5b7e886756f419a0177f68d9987571337bec
macbook kal <arnaud.pantenier@gmail.com>
2023-08-14 14:08:36 +0200

    Fin todolist

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+Todolist make with TypeScript
---
+++ src/components/InputField.tsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import "../style.css";
+
+interface props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return (
+        <form
+            className="formulaire"
+            onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.blur();
+            }}
+        >
+            <input
+                type="text"
+                placeholder="Enter a Task"
+                value={todo}
+                ref={inputRef}
+                onChange={(e) => setTodo(e.target.value)}
+                className="formulaire__input"
+            />
+            <button type="submit" className="formulaire__btn ">
+                +
+            </button>
+        </form>
+    );
+};
+
+export default InputField;
